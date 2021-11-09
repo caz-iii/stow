5551f81e5c00598e5ab00e3ef53a84b40f843bf4
caz-iii <zimmermancarl0@gmail.com>
2021-11-09 18:30:08 -0500

    "Tue Nov  9 06:30:08 PM EST 2021"

---
+++ .screenlayout/all-on.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+xrandr \
+    --output VGA-0 --mode 1920x1080 --rate 60.00 --pos 7040x0 --rotate normal \
+    --output LVDS-0 --mode 1920x1080 --pos 0x0 --rotate normal \
+    --output DP-0 --off \
+    --output HDMI-0 --mode 2560x1440 --rate 74.97 --pos 1920x0 --rotate normal \
+    --output DP-1 --primary --mode 2560x1440 --rate 74.97 --pos 4480x0 --rotate normal
---
+++ .screenlayout/double-docked.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+xrandr \
+    --output VGA-0 --off \
+    --output LVDS-0 --off \
+    --output DP-0 --off \
+    --output HDMI-0 --mode 2560x1440 --rate 74.97 --pos 1920x0 --rotate normal \
+    --output DP-1 --primary --mode 2560x1440 --rate 74.97 --pos 4480x0 --rotate normal
