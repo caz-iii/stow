95c1259b06092ca783a9ea90a40bfff81dc9a2b6
caz-iii <zimmermancarl0@gmail.com>
2021-11-09 18:53:53 -0500

    "Tue Nov  9 06:53:53 PM EST 2021"

--- .screenlayout/double-docked.sh
+++ .screenlayout/double-docked.sh
@@ -5,4 +5,6 @@ xrandr \
     --output DP-0 --off \
     --output HDMI-0 --mode 2560x1440 --rate 74.97 --pos 1920x0 --rotate normal \
     --output DP-1 --primary --mode 2560x1440 --rate 74.97 --pos 4480x0 --rotate normal
+sleep 1
 feh --bg-center ~/data/pictures/album-personal/wallpapers/dark.jpg 
+exit
