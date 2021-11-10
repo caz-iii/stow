49def537f075fe3ac5deb97b5e04579f406a0d76
caz-iii <zimmermancarl0@gmail.com>
2021-11-09 19:02:49 -0500

    "Tue Nov  9 07:02:49 PM EST 2021"

--- .screenlayout/all-on.sh
+++ .screenlayout/all-on.sh
@@ -5,3 +5,6 @@ xrandr \
     --output DP-0 --off \
     --output HDMI-0 --mode 2560x1440 --rate 74.97 --pos 1920x0 --rotate normal \
     --output DP-1 --primary --mode 2560x1440 --rate 74.97 --pos 4480x0 --rotate normal
+sleep 1
+nitrogen --restore
+exit
