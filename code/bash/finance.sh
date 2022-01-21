2151929d6c59243482d6c7af947f6606596b75e3
caz-iii <zimmermancarl0@gmail.com>
2022-01-21 09:15:50 -0500

    "Fri Jan 21 09:15:50 AM EST 2022"

--- code/bash/finance.sh
+++ code/bash/finance.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 chromium-browser-privacy --new-window https://www.wsfsbank.com https://www.capitalone.com https://www.chase.com https://online.citi.com/
+codium code/Hands-On-Microservices-with-Spring-Boot-and-Spring-Cloud
 exit
---
+++ code/bash/microservices.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+chromium-browser-privacy --new-window https://trello.com/b/Aq7GXIyK/microservices https://www.youtube.com/playlist?list=PLeLcvrwLe185prGhjUrFGQsOh_0MArR1P
+exit
