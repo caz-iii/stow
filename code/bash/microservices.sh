8f62dc1ff9b1da74f74349c2304609c96361b7da
caz-iii <zimmermancarl0@gmail.com>
2022-01-21 09:18:09 -0500

    "Fri Jan 21 09:18:09 AM EST 2022"

--- code/bash/microservices.sh
+++ code/bash/microservices.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 chromium-browser-privacy --new-window https://trello.com/b/Aq7GXIyK/microservices https://www.youtube.com/playlist?list=PLeLcvrwLe185prGhjUrFGQsOh_0MArR1P
+codium ~/code/Hands-On-Microservices-with-Spring-Boot-and-Spring-Cloud 
 exit
