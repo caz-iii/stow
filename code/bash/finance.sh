55879073539781de332f8f61d1907ecbba830553
caz-iii <zimmermancarl0@gmail.com>
2022-01-24 08:59:55 -0500

    "Mon Jan 24 08:59:55 AM EST 2022"

--- code/bash/finance.sh
+++ code/bash/finance.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 chromium-browser-privacy --new-window https://www.wsfsbank.com https://www.capitalone.com https://www.chase.com https://online.citi.com/
-codium code/Hands-On-Microservices-with-Spring-Boot-and-Spring-Cloud
 exit
