4daaa042101c8c8b035b7f833331cb4c27cfd26e
caz-iii <zimmermancarl0@gmail.com>
2022-02-09 00:03:06 -0500

    "Wed Feb  9 12:03:06 AM EST 2022"

--- .screenlayout/vga-off.sh
+++ .screenlayout/vga-off.sh
@@ -6,5 +6,5 @@ xrandr \
     --output HDMI-0 --mode 2560x1440 --rate 74.97 --pos 1920x0 --rotate normal \
     --output DP-1 --primary --mode 2560x1440 --rate 74.97 --pos 4480x0 --rotate normal
 sleep 1
-feh --bg-center ~/data/pictures/personal/wallpapers/nord-fish.png
+feh --bg-center ~/data/pictures/personal/wallpapers/truvada.png
 exit
