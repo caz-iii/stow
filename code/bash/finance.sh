1a317b2c11f1429caf8f15d49476c99634d35212
caz-iii <zimmermancarl0@gmail.com>
2022-03-02 22:52:08 -0500

    "Wed Mar  2 10:52:08 PM EST 2022"

--- code/bash/finance.sh
+++ code/bash/finance.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-chromium-browser-privacy --new-window https://www.wsfsbank.com https://www.capitalone.com https://www.chase.com https://online.citi.com/
+chromium-browser-privacy --new-window https://venmo.com https://www.wsfsbank.com https://www.chase.com https://www.capitalone.com https://online.citi.com/
 exit
