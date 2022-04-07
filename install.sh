0f41ed87d880b4dd80f158e0a81c6fbaa2837959
caz-iii <zimmermancarl0@gmail.com>
2022-04-07 14:09:40 -0400

    "Thu Apr  7 02:09:40 PM EDT 2022"

--- install.sh
+++ install.sh
@@ -39,35 +39,50 @@ sudo dnf install -y \
 # NPM and RPM Packages
 
 # taskbook
+echo "getting taskbook..."
 npm install --global taskbook
 
-# bitwarden
-wget https://github.com/bitwarden/desktop/releases/download/v1.32.1/Bitwarden-1.32.1-x86_64.rpm
-sudo rpm -i Bitwarden-1.32.1-x86_64.rpm
+echo "Downloading RPMs..."
 
 # onlyoffice
 wget https://download.onlyoffice.com/install/desktop/editors/linux/onlyoffice-desktopeditors.x86_64.rpm
-sudo rpm -i onlyoffice-desktopeditors.x86_64.rpm
+
+# bitwarden
+curl -s https://api.github.com/repos/bitwarden/desktop/releases/latest \
+    | grep "browser_download_url.*rpm" \
+    | cut -d : -f 2,3 \
+    | tr -d \" \
+    | wget -qi -
 
 # marktext
-wget https://github.com/marktext/marktext/releases/download/v0.17.1/marktext-x86_64.rpm
-sudo rpm -i marktext-x86_64.rpm
+curl -s https://api.github.com/repos/marktext/marktext/releases/latest \
+    | grep "browser_download_url.*rpm" \
+    | cut -d : -f 2,3 \
+    | tr -d \" \
+    | wget -qi -
+
+echo "Installing RPMs..."
+sudo rpm -i *.rpm
 
 # vscodium
+echo "getting vscodium..."
 sudo rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
 printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h" | sudo tee -a /etc/yum.repos.d/vscodium.repo
 sudo dnf install -y codium
 
 # librewolf
+echo "getting librewolf..."
 sudo rpm --import https://keys.openpgp.org/vks/v1/by-fingerprint/034F7776EF5E0C613D2F7934D29FBD5F93C0CFC3
 sudo dnf config-manager --add-repo https://rpm.librewolf.net
 sudo dnf install -y librewolf
 
 # Ungoogled Chromium (Fedora 34)
+echo "getting ungoogled chromium..."
 sudo dnf config-manager -add-repo https://download.opensuse.org/repositories/home:/ungoogled_chromium/Fedora_34/home:ungoogled_chromium.repo
 sudo dnf install -y ungoogled-chromium
 
 # Ungoogled Chromium (Fedora 35)
+# echo "getting ungoogled chromium..."
 # dnf config-manager --add-repo https://download.opensuse.org/repositories/home:/ungoogled_chromium/Fedora_35/home:ungoogled_chromium.repo
 # dnf install ungoogled-chromium
 
