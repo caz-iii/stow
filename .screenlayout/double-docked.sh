69d0fe0c7c7a7a596b17ae8419e67547516cc85b
Carl <zimmermancarl0@gmail.com>
2022-04-10 14:20:25 -0400

    "Sun Apr 10 02:20:25 PM EDT 2022"

--- .screenlayout/double-docked.sh
+++ .screenlayout/double-docked.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 xrandr
 xrandr \
-    --output VGA-1 --off \
-    --output LVDS-1 --off \
-    --output HDMI-1 --mode 2560x1440 --rate 74.97 --pos 1920x0 --rotate normal \
+    --output VGA-0 --off \
+    --output LVDS-0 --off \
+    --output DP-0 --off \
+    --output HDMI-0 --mode 2560x1440 --rate 74.97 --pos 1920x0 --rotate normal \
     --output DP-1 --primary --mode 2560x1440 --rate 74.97 --pos 4480x0 --rotate normal
 sleep 1
 feh --bg-center ~/data/pictures/personal/wallpapers/truvada.png
