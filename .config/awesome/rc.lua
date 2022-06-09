935d9bc06df4415edccf09a57137e793a8d3b569
caz <codingwithcarl@outlook.com>
2022-06-08 22:16:33 -0400

    "Wed Jun  8 10:16:33 PM EDT 2022"

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -110,7 +110,7 @@ awful.util.terminal = terminal
 awful.util.tagnames = { "1", "2", "3", "4", "5", "6", "7", "8", "9"}
 awful.layout.layouts = {
     awful.layout.suit.tile,
-    --awful.layout.suit.floating,
+    awful.layout.suit.floating,
     --awful.layout.suit.tile.left,
     --awful.layout.suit.tile.bottom,
     --awful.layout.suit.tile.top,
