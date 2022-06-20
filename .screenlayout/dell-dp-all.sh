520a02c7b748c6c9bdc98a0250be3bbc33dd8606
caz <codingwithcarl@outlook.com>
2022-06-20 17:10:03 -0400

    "Mon Jun 20 05:10:03 PM EDT 2022"

--- .screenlayout/dell-dp-all.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output eDP-1 --primary --mode 1920x1200 --pos 2880x1440 --rotate normal --output DP-1 --mode 2560x1440 --rate 74.97 --pos 5120x0 --rotate normal --output DP-2 --mode 2560x1440 --rate 74.97 --pos 0x0 --rotate normal --output DP-3 --mode 2560x1440 --rate 74.97 --pos 2560x0 --rotate normal
